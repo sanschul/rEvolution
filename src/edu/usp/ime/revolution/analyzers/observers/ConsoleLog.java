0ccfe69ec00506128e9ca412d18b0630a0d2114f
Mauricio Aniche <mauricioaniche@gmail.com>
2010-05-24 23:20:19 -0300

    improving console log

--- src/edu/usp/ime/revolution/analyzers/observers/ConsoleLog.java
+++ src/edu/usp/ime/revolution/analyzers/observers/ConsoleLog.java
@@ -1,14 +1,20 @@
 package edu.usp.ime.revolution.analyzers.observers;
 
+import java.util.Map;
+
 import edu.usp.ime.revolution.metrics.MetricSet;
 import edu.usp.ime.revolution.scm.ChangeSet;
 
 public class ConsoleLog implements AnalyzerObserver {
 
 	public void notify(ChangeSet cs, MetricSet set) {
-		System.out.println("---------------------------------------");
 		System.out.println("CS: " + cs.getId() + " " + cs.getTime());
 		System.out.println("Number of metrics: " + set.getMetrics().size());
+		
+		for(Map.Entry<String, Double> metric: set.getMetrics().entrySet()) {
+			System.out.println("-> " + metric.getKey() + "=" + metric.getValue());	
+		}
+		
 		System.out.println("---------------------------------------");
 	}
 
