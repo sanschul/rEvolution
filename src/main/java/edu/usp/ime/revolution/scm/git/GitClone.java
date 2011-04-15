3cd4ab40cba80761165ca0ecf01ec427b6d6e150
Mauricio Aniche <mauricioaniche@gmail.com>
2011-04-15 16:21:39 -0300

    git clone agora

---
+++ src/main/java/edu/usp/ime/revolution/scm/git/GitClone.java
@@ -0,0 +1,33 @@
+package edu.usp.ime.revolution.scm.git;
+
+import java.util.List;
+
+import edu.usp.ime.revolution.executor.CommandExecutor;
+import edu.usp.ime.revolution.scm.ChangeSet;
+import edu.usp.ime.revolution.scm.ChangeSetInfo;
+import edu.usp.ime.revolution.scm.SCM;
+import edu.usp.ime.revolution.scm.SCMException;
+
+public class GitClone implements SCM{
+
+	private final Git git;
+
+	public GitClone(String gitRepository, String destiny, CommandExecutor exec, Git git) {
+		this.git = git;
+		
+		try {
+			exec.runCommand("git clone " + gitRepository + " " + destiny);
+		} catch (Exception e) {
+			throw new SCMException(e);
+		}
+	}
+	
+	public List<ChangeSetInfo> getChangeSetList() {
+		return git.getChangeSetList();
+	}
+
+	public ChangeSet getChangeSet(ChangeSetInfo cs) {
+		return git.getChangeSet(cs);
+	}
+
+}
