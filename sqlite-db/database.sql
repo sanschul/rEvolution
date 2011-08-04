87d7ecccdce68fcb46f211991f4cedf86a665815
Mauricio Aniche <mauricioaniche@gmail.com>
2011-08-04 12:01:22 -0300

    removing sqlite db - it is not used anymore

--- sqlite-db/database.sql
+++
@@ -1,2 +0,0 @@
-create table metrics (id integer primary key autoincrement, name varchar(200), value double, target varchar(200), level varchar(200), tool varchar(200), changeset_id integer);
-create table changesets (id integer primary key autoincrement, project varchar(200), name varchar(200), date datetime);
--- src/main/java/br/com/caelum/revolution/builds/SpecificBuildFactory.java
+++ src/main/java/br/com/caelum/revolution/builds/SpecificBuildFactory.java
@@ -1,7 +1,6 @@
 package br.com.caelum.revolution.builds;
 
 import br.com.caelum.revolution.config.Config;
-import br.com.caelum.revolution.scm.SCM;
 
 public interface SpecificBuildFactory {
 	Build build(Config config);
