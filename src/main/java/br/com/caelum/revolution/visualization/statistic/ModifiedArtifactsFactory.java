5b753cc35f63f518a69ab5bedba19e475991561c
Mauricio Aniche <mauricioaniche@gmail.com>
2011-08-15 15:37:59 -0300

    wrong title

--- src/main/java/br/com/caelum/revolution/visualization/statistic/ModifiedArtifactsFactory.java
+++ src/main/java/br/com/caelum/revolution/visualization/statistic/ModifiedArtifactsFactory.java
@@ -12,7 +12,7 @@ public class ModifiedArtifactsFactory implements SpecificVisualizationFactory {
 
 	public Visualization build(Config config) {
 		return new GroupedDataVisualization(
-				new PieChart("Commits per Developer", new File(config.asString("file")), 1500, 1500), 
+				new PieChart("Most Modified Artifacts", new File(config.asString("file")), 1500, 1500), 
 				config.asInt("threshold"),
 				"select a.name, count(1) qty from artifact a inner join modification m on m.artifact_id = a.id group by a.name order by qty desc");
 	}
