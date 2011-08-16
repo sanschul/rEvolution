3932349d5a4ad054e02c434067156cbc4d1f689d
Mauricio Aniche <mauricioaniche@gmail.com>
2011-08-16 16:11:06 -0300

    LinesAddedPerCommitOverTimeFactory

---
+++ src/main/java/br/com/caelum/revolution/visualization/evolution/LinesAddedPerCommitOverTimeFactory.java
@@ -0,0 +1,20 @@
+package br.com.caelum.revolution.visualization.evolution;
+
+import java.io.File;
+import java.math.BigDecimal;
+
+import br.com.caelum.revolution.config.Config;
+import br.com.caelum.revolution.visualization.SpecificVisualizationFactory;
+import br.com.caelum.revolution.visualization.Visualization;
+import br.com.caelum.revolution.visualization.common.GroupedDataVisualization;
+import br.com.caelum.revolution.visualization.common.LineChart;
+
+public class LinesAddedPerCommitOverTimeFactory implements SpecificVisualizationFactory {
+
+	public Visualization build(Config config) {
+		return new GroupedDataVisualization<BigDecimal>(
+				new LineChart("Lines Added Per Commit over Time", "Commit", "Number of Added Lines", new File(config.asString("file")), 3000, 1500), 
+				"select sum(linesAdded) qty, convert(commit_id, char) name from  lineschangedcount l inner join commit c on l.commit_id = c.id group by commit_id order by commit_id");
+	}
+
+}
