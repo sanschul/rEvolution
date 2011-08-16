a3a96212564eb59b515129da9d83cc1ec96f1586
Mauricio Aniche <mauricioaniche@gmail.com>
2011-08-16 00:28:37 -0300

    rename

--- src/main/java/br/com/caelum/revolution/visualization/statistic/BugsPerWeekDayFactory.java
+++ src/main/java/br/com/caelum/revolution/visualization/statistic/BugsPerWeekDayFactory.java
@@ -6,13 +6,13 @@ import br.com.caelum.revolution.config.Config;
 import br.com.caelum.revolution.visualization.SpecificVisualizationFactory;
 import br.com.caelum.revolution.visualization.Visualization;
 import br.com.caelum.revolution.visualization.common.BarChart;
-import br.com.caelum.revolution.visualization.common.ThresholdGroupedDataVisualization;
+import br.com.caelum.revolution.visualization.common.ThresholdedGroupedDataVisualization;
 
 public class BugsPerWeekDayFactory implements SpecificVisualizationFactory {
 
 	public Visualization build(Config config) {
 
-		return new ThresholdGroupedDataVisualization(
+		return new ThresholdedGroupedDataVisualization(
 				new BarChart("Bugged Artifacts", "Bugs per Week Day", "Quantity", new File(config.asString("file")), 1500, 1500),
 				7,
 				"select dayname(c.date) name, count(1) qty from bugorigin bo inner join commit c on c.id = bo.buggedCommit_id group by dayname(c.date) order by dayofweek(c.date)");
--- src/main/java/br/com/caelum/revolution/visualization/statistic/CommitsPerDeveloperFactory.java
+++ src/main/java/br/com/caelum/revolution/visualization/statistic/CommitsPerDeveloperFactory.java
@@ -5,13 +5,13 @@ import java.io.File;
 import br.com.caelum.revolution.config.Config;
 import br.com.caelum.revolution.visualization.SpecificVisualizationFactory;
 import br.com.caelum.revolution.visualization.Visualization;
-import br.com.caelum.revolution.visualization.common.ThresholdGroupedDataVisualization;
+import br.com.caelum.revolution.visualization.common.ThresholdedGroupedDataVisualization;
 import br.com.caelum.revolution.visualization.common.PieChart;
 
 public class CommitsPerDeveloperFactory implements SpecificVisualizationFactory {
 
 	public Visualization build(Config config) {
-		return new ThresholdGroupedDataVisualization(
+		return new ThresholdedGroupedDataVisualization(
 				new PieChart("Commits per Developer", new File(config.asString("file")), 1500, 1500), 
 				config.asInt("threshold"),
 				"select a.name, count(1) qty from author a inner join commit c on c.author_id = a.id group by a.name order by qty desc");
